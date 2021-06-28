d672095e92bb2a6d54313da359453b2daa363b5d
Joshua <jtjackson325@gmail.com>
2021-06-27 22:11:05 -0600

    got the folder for lesson 9.5 created that will hold the index.html file

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const inquirer = require('inquirer');
+const { prototype } = require('events');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -125,17 +128,10 @@ const promptUser = () => {
   };
 
 promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-        console.log(portfolioData);
-    });
-//const fs = require('fs');
-//const generatePage = require('./src/page-template.js');
+  .then(promptProject)
+  .then(portfolioData => {
+    const pageHTML = generatePage(portfolioData);
 
-//const pageHTML = generatePage(name, github);
+    fs.writeFile('./dist/index.html');
 
-//fs.writeFile('index.html', generatePage(name, github), err => {
-    //if (err) throw err;
-
-    //console.log('Portfolio complete! Check out the index.html to see the output!');
-//});
+  });
